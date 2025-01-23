ac388cb2d7f26374f28e97015b35676cabe65a40
Rakshan Shetty <rakshanrakshu20@Gmail.com>
2025-01-23 15:48:05 +0530

    Toolbar Modifed

--- src/components/PersonalEditor.jsx
+++ src/components/PersonalEditor.jsx
@@ -1,82 +1,117 @@
-import { useEffect, useRef } from 'react';
-import ReactQuill from 'react-quill';
-import 'react-quill/dist/quill.snow.css'; // Import Quill styles
+import { useEffect, useRef } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css"; // Import Quill styles
 
 // Personal Editor Component
 const PersonalEditor = () => {
-  const quillRef = useRef(null);
+	const quillRef = useRef(null);
 
-  useEffect(() => {
-    const savedContent = localStorage.getItem('personalContent');
-    if (savedContent && quillRef.current) {
-      const quillInstance = quillRef.current.getEditor();
-      quillInstance.setContents(JSON.parse(savedContent));
-    }
+	useEffect(() => {
+		const savedContent = localStorage.getItem("personalContent");
+		if (savedContent && quillRef.current) {
+			const quillInstance = quillRef.current.getEditor();
+			quillInstance.setContents(JSON.parse(savedContent));
+		}
 
-    // Save content to localStorage on text change
-    const quillInstance = quillRef.current ? quillRef.current.getEditor() : null;
-    if (quillInstance) {
-      quillInstance.on('text-change', () => {
-        localStorage.setItem('personalContent', JSON.stringify(quillInstance.getContents()));
-      });
-    }
+		// Save content to localStorage on text change
+		const quillInstance = quillRef.current
+			? quillRef.current.getEditor()
+			: null;
+		if (quillInstance) {
+			quillInstance.on("text-change", () => {
+				localStorage.setItem(
+					"personalContent",
+					JSON.stringify(quillInstance.getContents())
+				);
+			});
+		}
+	}, []);
 
-  }, []);
+	const modules = {
+		toolbar: {
+			container: "#personalToolbar", // Point to the custom toolbar
+		},
+	};
 
-  const modules = {
-    toolbar: {
-      container: '#personalToolbar', // Point to the custom toolbar
-    },
-  };
+	return (
+		<div className='flex flex-col h-screen'>
+			{/* Toolbar */}
+			<div
+				id='personalToolbar'
+				className='flex justify-center gap-2 p-2 bg-gray-100 rounded-t-lg overflow-x-auto
+ '
+			>
+				{/* Font Size Icons with Font Awesome */}
+				<div className='flex gap-2'>
+					<button
+						className='ql-size'
+						value='small'
+						aria-label='Small Font Size'
+						title='Small'
+					>
+						<i className='fas fa-font fa-xs'></i> {/* Small Font Size Icon */}
+					</button>
 
-  return (
-    <div className="flex flex-col h-screen">
-      {/* Toolbar */}
-      <div
-        id="personalToolbar"
-        className="flex justify-center gap-2 p-2 bg-gray-100 rounded-t-lg overflow-x-auto
- "
-      >
-        <button className="ql-bold text-lg font-bold">B</button>
-        <button className="ql-italic text-lg font-italic">I</button>
-        <button className="ql-underline text-lg underline">U</button>
-        <button className="ql-strike text-lg line-through">S</button>
-        <button className="ql-script" value="super">Super</button>
-        <button className="ql-script" value="sub">Sub</button>
+					<button
+						className='ql-size'
+						value='large'
+						aria-label='Large Font Size'
+						title='Large'
+					>
+						<i className='fas fa-font fa-sm'></i> {/* Normal Font Size Icon */}
+					</button>
+					<button
+						className='ql-size'
+						value='huge'
+						aria-label='Huge Font Size'
+						title='Huge'
+					>
+						<i className='fas fa-font fa-lg'></i> {/* Large Font Size Icon */}
+					</button>
+				</div>
 
-        {/* Font Size Dropdown */}
-        <select className="ql-size" defaultValue="normal">
-          <option value="small">Small</option>
-          <option value="normal">Normal</option>
-          <option value="large">Large</option>
-          <option value="huge">Huge</option>
-        </select>
+				<button className='ql-bold text-lg font-bold'>B</button>
+				<button className='ql-italic text-lg font-italic'>I</button>
+				<button className='ql-underline text-lg underline'>U</button>
+				<button className='ql-strike text-lg line-through'>S</button>
+				<button className='ql-script' value='super'>
+					Super
+				</button>
+				<button className='ql-script' value='sub'>
+					Sub
+				</button>
 
-        <button className="ql-list" value="ordered">Ordered List</button>
-        <button className="ql-list" value="bullet">Bullet List</button>
-        <button className="ql-indent" value="-1">Indent</button>
-        <button className="ql-indent" value="+1">Outdent</button>
+				<button className='ql-list' value='ordered'>
+					Ordered List
+				</button>
+				<button className='ql-list' value='bullet'>
+					Bullet List
+				</button>
+				<button className='ql-indent' value='-1'>
+					Indent
+				</button>
+				<button className='ql-indent' value='+1'>
+					Outdent
+				</button>
 
-        <button className="ql-link">Link</button>
-        <button className="ql-image">Image</button>
-        <button className="ql-code-block">Code Block</button>
-        <button className="ql-blockquote">Blockquote</button>
-        <button className="ql-clean">Clear Formatting</button>
-      </div>
+				<button className='ql-link'>Link</button>
+				<button className='ql-image'>Image</button>
+				<button className='ql-code-block'>Code Block</button>
+				<button className='ql-blockquote'>Blockquote</button>
+				<button className='ql-clean'>Clear Formatting</button>
+			</div>
 
-      {/* Editor */}
-      <div
-        className="flex-1 h-full border border-gray-300 rounded-lg bg-white shadow-md p-2"
-      >
-        <ReactQuill
-          ref={quillRef}
-          theme="snow"
-          modules={modules}
-          style={{ height: '100%' }}
-        />
-      </div>
-    </div>
-  );
+			{/* Editor */}
+			<div className='flex-1 h-full border border-gray-300 rounded-lg bg-white shadow-md p-2'>
+				<ReactQuill
+					ref={quillRef}
+					theme='snow'
+					modules={modules}
+					style={{ height: "100%" }}
+				/>
+			</div>
+		</div>
+	);
 };
 
 export default PersonalEditor;
--- src/components/SharedEditor.jsx
+++ src/components/SharedEditor.jsx
@@ -7,82 +7,108 @@ import { QuillBinding } from "y-quill";
 
 // Shared Editor Component
 const SharedEditor = () => {
-  const quillRef = useRef(null);
+	const quillRef = useRef(null);
 
-  useEffect(() => {
-    const sharedDoc = new Y.Doc();
-    const sharedProvider = new WebrtcProvider("sharedRoom", sharedDoc);
-    const sharedType = sharedDoc.getText("quill");
+	useEffect(() => {
+		const sharedDoc = new Y.Doc();
+		const sharedProvider = new WebrtcProvider("sharedRoom", sharedDoc);
+		const sharedType = sharedDoc.getText("quill");
 
-    if (quillRef.current) {
-      const quillInstance = quillRef.current.getEditor();
-      new QuillBinding(sharedType, quillInstance, sharedProvider.awareness);
-    }
+		if (quillRef.current) {
+			const quillInstance = quillRef.current.getEditor();
+			new QuillBinding(sharedType, quillInstance, sharedProvider.awareness);
+		}
 
-    // Cleanup
-    return () => {
-      sharedProvider.destroy();
-    };
-  }, []);
+		// Cleanup
+		return () => {
+			sharedProvider.destroy();
+		};
+	}, []);
 
-  const modules = {
-    toolbar: {
-      container: "#sharedToolbar", // Point to the custom toolbar
-    },
-  };
+	const modules = {
+		toolbar: {
+			container: "#sharedToolbar", // Point to the custom toolbar
+		},
+	};
 
-  return (
-    <div className="flex flex-col h-screen">
-      {/* Toolbar */}
-      <div
-        id="sharedToolbar"
-        className="flex justify-center gap-2 p-2 bg-gray-100 rounded-t-lg overflow-x-auto
-"
-      >
-        <button className="ql-bold text-lg font-bold">B</button>
-        <button className="ql-italic text-lg font-italic">I</button>
-        <button className="ql-underline text-lg underline">U</button>
-        <button className="ql-strike text-lg line-through">S</button>
-        <button className="ql-script" value="super">Super</button>
-        <button className="ql-script" value="sub">Sub</button>
-        
-        {/* Font Size Dropdown */}
-        <select className="ql-size" defaultValue="normal">
-          <option value="small">Small</option>
-          <option value="normal">Normal</option>
-          <option value="large">Large</option>
-          <option value="huge">Huge</option>
-        
-        </select>
-        
-        <button className="ql-list" value="ordered">Ordered List</button>
-        <button className="ql-list" value="bullet">Bullet List</button>
-        <button className="ql-indent" value="-1">Indent</button>
-        <button className="ql-indent" value="+1">Outdent</button>
-        
-     
-        
-        <button className="ql-link">Link</button>
-        <button className="ql-image">Image</button>
-     
-        <button className="ql-code-block">Code Block</button>
-        <button className="ql-blockquote">Blockquote</button>
-        <button className="ql-clean">Clear Formatting</button>
-      </div>
+	return (
+		<div className='flex flex-col h-screen'>
+			{/* Toolbar */}
+			<div
+				id='sharedToolbar'
+				className='flex justify-center gap-2 p-2 bg-gray-100 rounded-t-lg overflow-x-auto
+'
+			>
+						<div className='flex gap-2'>
+					<button
+						className='ql-size'
+						value='small'
+						aria-label='Small Font Size'
+						title='Small'
+					>
+						<i className='fas fa-font fa-xs'></i> {/* Small Font Size Icon */}
+					</button>
 
-      {/* Editor */}
-      <div
-        className="flex-1 h-full border border-gray-300  bg-white shadow-md p-2"
-      >
-        <ReactQuill
-          ref={quillRef}
-          theme="snow"
-          modules={modules}
-          style={{ height: '100%' }} 
-        />
-      </div>
-    </div>
-  );
+					<button
+						className='ql-size'
+						value='large'
+						aria-label='Large Font Size'
+						title='Large'
+					>
+						<i className='fas fa-font fa-sm'></i> {/* Normal Font Size Icon */}
+					</button>
+					<button
+						className='ql-size'
+						value='huge'
+						aria-label='Huge Font Size'
+						title='Huge'
+					>
+						<i className='fas fa-font fa-lg'></i> {/* Large Font Size Icon */}
+					</button>
+				</div>
+				<button className='ql-bold text-lg font-bold'>B</button>
+				<button className='ql-italic text-lg font-italic'>I</button>
+				<button className='ql-underline text-lg underline'>U</button>
+				<button className='ql-strike text-lg line-through'>S</button>
+				<button className='ql-script' value='super'>
+					Super
+				</button>
+				<button className='ql-script' value='sub'>
+					Sub
+				</button>
+
+				<button className='ql-list' value='ordered'>
+					Ordered List
+				</button>
+				<button className='ql-list' value='bullet'>
+					Bullet List
+				</button>
+				<button className='ql-indent' value='-1'>
+					Indent
+				</button>
+				<button className='ql-indent' value='+1'>
+					Outdent
+				</button>
+
+				<button className='ql-link'>Link</button>
+				<button className='ql-image'>Image</button>
+
+				<button className='ql-code-block'>Code Block</button>
+				<button className='ql-blockquote'>Blockquote</button>
+				<button className='ql-clean'>Clear Formatting</button>
+			</div>
+
+			{/* Editor */}
+			<div className='flex-1 h-full border border-gray-300  bg-white shadow-md p-2'>
+				<ReactQuill
+					ref={quillRef}
+					theme='snow'
+					modules={modules}
+					style={{ height: "100%" }}
+				/>
+			</div>
+		</div>
+	);
 };
 
 export default SharedEditor;
